ef7f089f02daa5f65ba0407f337c79537155f2f7
kupets <kupets@evergreens.in.ua>
2025-08-05 00:54:48 +0300

    feat: create form model

---
+++ src/models/form.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const formSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+}, { timestamps: true });
+
+const Form = mongoose.model('Form', formSchema);
+
+module.exports = Form;
