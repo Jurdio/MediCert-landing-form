aeeda5939730b4f98705280905ac9ac69c71899c
kupets <kupets@evergreens.in.ua>
2025-08-05 21:32:18 +0300

    add 3000 port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const swaggerSpec = require('./src/config/swagger');
 const demoRoute = require('./routes/demo.route');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 connectDB();
 
